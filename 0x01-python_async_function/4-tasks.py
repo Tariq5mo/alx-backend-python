816f140c9d21287e58eb98d27dac18dfb092cf79
Tariq5mo <tariqomer756@gmail.com>
2024-11-17 13:42:14 +0200

    Fix task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,8 @@ task_wait_random n times with the specified max_delay.
 
 Functions:
     task_wait_n(n: int, max_delay: int) -> List[float]:
-        Spawns task_wait_random n times with the specified max_delay and returns
+        Spawns task_wait_random n times with the specified max_delay and
+        returns
         the list of all the delays (float values) in ascending order without
         using sort() because of concurrency.
 """
@@ -19,18 +20,18 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Spawns task_wait_random n times with the specified max_delay.
 
     Args:
-        n (int): The number of times to spawn task_wait_random.
-        max_delay (int): The maximum delay value for task_wait_random.
+    n (int): The number of times to spawn task_wait_random.
+    max_delay (int): The maximum delay value for task_wait_random.
 
     Returns:
-        List[float]: A list of all the delays (float values)
-        in ascending order.
+    List[float]: A list of all the delays (float values)
+    in ascending order.
     """
     list_tasks = [task_wait_random(max_delay) for i in range(n)]
     list_delays = []
 
     for task in asyncio.as_completed(list_tasks):
         delay = await task
-        list_delays.append(delay)
+    list_delays.append(delay)
 
     return list_delays
