34af41fc44c61c4c737143f048c010e9e8e40c10
Tariq5mo <tariqomer756@gmail.com>
2024-11-17 14:57:42 +0200

    Fix task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,10 +13,10 @@ Functions:
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Loops 10 times, each time asynchronously waits 1 second,
     then yields a random number between 0 and 10.
@@ -24,6 +24,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Yields:
         float: A random number between 0 and 10.
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
